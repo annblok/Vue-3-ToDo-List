c52af6b6948e7347947b9892d9a4612307139ebb
Anna Blok <anya14_93@mail.ru>
2021-02-01 18:09:12 +0500

    add files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Vue-3-ToDo-List
+# A simple ToDo App with Vue 3
+
+*Demo:  [https://annblok.github.io/Vue-3-ToDo-List](https://annblok.github.io/Vue-3-ToDo-List/)*
---
+++ app.js
@@ -0,0 +1,37 @@
+Vue.createApp({
+  data() {
+    return {
+      valueInput: '',
+      needDoList: [],
+      completeList: []
+    };
+  },
+  methods: {
+    handleInput (event) {
+      this.valueInput = event.target.value;
+    },
+    addTask () {
+      if(this.valueInput === '') { return };
+      this.needDoList.push({
+        title: this.valueInput,
+        id: Math.random()
+      });
+      this.valueInput = '';
+    },
+    doCheck (index, type) {
+
+      if(type === 'need') {
+        const completeMask = this.needDoList.splice(index, 1);
+        this.completeList.push(completeMask);
+      } else {
+        const noCompleteMask = this.completeList.splice(index, 1);
+        this.needDoList.push(noCompleteMask);
+      }
+    },
+    removeMask (index, type) {
+      const toDoList = type === 'need' ? this.needDoList : this.completeList;
+      toDoList.splice(index, 1);
+    }
+  }
+}
+).mount('#app');
